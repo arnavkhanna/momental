437aba9a9453ca361bbccd898ad673436dfbf22e
Gina Roberg <ginaroberg@gmail.com>
2022-05-09 15:21:51 -0700

    with buttons

--- momental-app/src/App.js
+++ momental-app/src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Button } from 'antd';
+import { Input, Space } from 'antd';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import { UserOutlined } from '@ant-design/icons';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Momental 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Space direction="vertical">
+          <Input placeholder="input email" prefix={<UserOutlined />} />
+        </Space>
+        <Space direction="vertical">
+          <Input.Password
+            placeholder="input password"
+            iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+         />
+        </Space>
+        <Space direction="vertical">
+        <Button type="primary">Log-in</Button>
+        </Space>
+        
       </header>
     </div>
   );
