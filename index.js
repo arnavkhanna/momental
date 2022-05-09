0db4a49ac10de006892a38a4ec171fcdba02e855
Gina Roberg <ginaroberg@gmail.com>
2022-05-09 12:46:56 -0700

    updated log in

--- index.js
+++ index.js
@@ -36,20 +36,7 @@ function register_user(email,password){
         console.log(errorMessage)
     });
 } 
-function auth_email (email){
-    sendSignInLinkToEmail(auth, email, actionCodeSettings)
-    .then(() => {
-        // The link was successfully sent. Inform the user.
-        // Save the email locally so you don't need to ask the user for it again
-        // if they open the link on the same device.
-        window.localStorage.setItem('emailForSignIn', email);
-        // ...
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ...
-    });
-}
 
-auth_email("jisolp7@gmail.com")
+
+
+register_user("groberg@ucsd.edu", "apple123");
