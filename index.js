481500296c6167f6a349fc90fb598059406a21f2
JISOLPARK <92129615+JISOLPARK@users.noreply.github.com>
2022-05-09 12:55:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -53,42 +53,6 @@ function register_user(email,password){
         console.log(errorMessage)
     });
 } 
-<<<<<<< HEAD
-=======
-
-register_user("doanbatung2000@gmail.com", "abcdefgh")
-
-function auth_email (email){
-    const actionCodeSettings = {
-        // URL you want to redirect back to. The domain (www.example.com) for this
-        // URL must be in the authorized domains list in the Firebase Console.
-        url: 'https://momental-bfee4.firebaseapp.com/__/auth/action?mode=action&oobCode=code',
-        // This must be true.
-        handleCodeInApp: true,
-        iOS: {
-          bundleId: 'com.example.ios'
-        },
-        android: {
-          packageName: 'com.example.android',
-          installApp: true,
-          minimumVersion: '12'
-        },
-    };
-    sendSignInLinkToEmail(auth, email, actionCodeSettings)
-    .then(() => {
-        // The link was successfully sent. Inform the user.
-        // Save the email locally so you don't need to ask the user for it again
-        // if they open the link on the same device.
-        window.localStorage.setItem('emailForSignIn', email);
-        // ...
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.error(error);
-    });
-}
->>>>>>> bf5f55614842a7a9d9829ab7c4ad279e13833683
 
 
 
