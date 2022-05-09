48057b79fc65bf207b4bdb7dedcac55e7377c618
JISOLPARK <92129615+JISOLPARK@users.noreply.github.com>
2022-05-09 16:02:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,41 +19,56 @@ const firebaseConfig = {
     measurementId: "G-MFMYW8QQPL"
   };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const auth = getAuth();
+auth.languageCode = 'it';
 
-//
-function register_user(email,password){
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        const actionCodeSettings = {
-            // URL you want to redirect back to. The domain (www.example.com) for this
-            // URL must be in the authorized domains list in the Firebase Console.
-            url: 'https://momental-bfee4.firebaseapp.com/__/auth/action?mode=action&oobCode=code',
-            // This must be true.
-            handleCodeInApp: true,
-            iOS: {
-              bundleId: 'com.example.ios'
-            },
-            android: {
-              packageName: 'com.example.android',
-              installApp: true,
-              minimumVersion: '12'
-            },
-            
-        };
-        sendEmailVerification(user, actionCodeSettings).catch(error => console.error(error));
-    })
-    .catch((error) => {
+window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
+  'size': 'invisible',
+  'callback': (response) => {
+    // reCAPTCHA solved, allow signInWithPhoneNumber.
+    onSignInSubmit();
+  }
+}, auth);
+
+window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {}, auth);
+
+window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
+  'size': 'normal',
+  'callback': (response) => {
+    // reCAPTCHA solved, allow signInWithPhoneNumber.
+    // ...
+  },
+  'expired-callback': () => {
+    // Response expired. Ask user to solve reCAPTCHA again.
+    // ...
+  }
+}, auth);
+
+const phoneNumber = getPhoneNumberFromUserInput();
+const appVerifier = window.recaptchaVerifier;
+
+function register_user(phoneNumber,appVerifier){
+  signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+      .then((confirmationResult) => {
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        window.confirmationResult = confirmationResult;
+        // ...
+      }).catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log(errorMessage)
-    });
-} 
+      });
+}
 
+confirmationResult.confirm(code).then((result) => {
+  // User signed in successfully.
+  const user = result.user;
+  // ...
+}).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage)
+});
 
-
-register_user("groberg@ucsd.edu", "apple123");
+register_user("6197720624","")
